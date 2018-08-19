8d98b75f5bcd9d0dc9f2b85ce1a828371f1baf4b
Matt <matthewgstillman@gmail.com>
2018-08-19 13:12:29 -0700

    Added Birds, Dogs, Reptiles

--- apps/petfinder_api/urls.py
+++ apps/petfinder_api/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^bird_breeds$', views.bird_breeds, name="bird_breeds"),
     url(r'^cat_breeds$', views.cat_breeds, name="cat_breeds"),
+    url(r'^dog_breeds$', views.dog_breeds, name="dog_breeds"),
+    url(r'^reptile_breeds$', views.reptile_breeds, name="reptile_breeds"),
 ]
--- apps/petfinder_api/views.py
+++ apps/petfinder_api/views.py
@@ -5,6 +5,31 @@ import petpy
 import json
 
 # Create your views here.
+def bird_breeds(request):
+    bird_breeds = []
+    Petfinder = petpy.api.Petfinder('a2a128f9fc017e107d12f400c579ba54')
+    breed_list = Petfinder.breed_list('bird', outputformat='json', return_df=False)
+    print breed_list
+    breed = breed_list['petfinder']['breeds']['breed']
+    print breed
+    petfinder = breed_list['petfinder']
+    print petfinder
+    breeds = petfinder['breeds']
+    print breeds
+    breed_type = breeds['breed']
+    print breed_type
+    breed_name = breed_type
+    print breed_name
+    for breed in breed_name:
+        print breed['$t']
+        bird_breeds.append(str(breed['$t']))
+    print bird_breeds
+    context = {
+        'breed_list': breed_list,
+        'bird_breeds': bird_breeds,
+    }
+    return render(request, 'petfinder_api/bird_breeds.html', context)
+
 def cat_breeds(request):
     cat_breeds = []
     Petfinder = petpy.api.Petfinder('a2a128f9fc017e107d12f400c579ba54')
@@ -30,6 +55,31 @@ def cat_breeds(request):
     }
     return render(request, 'petfinder_api/cat_breeds.html', context)
 
+def dog_breeds(request):
+    dog_breeds = []
+    Petfinder = petpy.api.Petfinder('a2a128f9fc017e107d12f400c579ba54')
+    breed_list = Petfinder.breed_list('dog', outputformat='json', return_df=False)
+    print breed_list
+    breed = breed_list['petfinder']['breeds']['breed']
+    print breed
+    petfinder = breed_list['petfinder']
+    print petfinder
+    breeds = petfinder['breeds']
+    print breeds
+    breed_type = breeds['breed']
+    print breed_type
+    breed_name = breed_type
+    print breed_name
+    for breed in breed_name:
+        print breed['$t']
+        dog_breeds.append(str(breed['$t']))
+    print cat_breeds
+    context = {
+        'breed_list': breed_list,
+        'dog_breeds': dog_breeds,
+    }
+    return render(request, 'petfinder_api/cat_breeds.html', context)
+
 
 def index(request):
     api = petfinder.PetFinderClient(api_key='a2a128f9fc017e107d12f400c579ba54', api_secret='5fb1ed325a37e2ce66c1a4600f652a70')
@@ -42,3 +92,29 @@ def index(request):
         'pet_array': pet_array,
     }
     return render(request, 'petfinder_api/index.html',context)
+
+
+def reptile_breeds(request):
+    reptile_breeds = []
+    Petfinder = petpy.api.Petfinder('a2a128f9fc017e107d12f400c579ba54')
+    breed_list = Petfinder.breed_list('reptile', outputformat='json', return_df=False)
+    print breed_list
+    breed = breed_list['petfinder']['breeds']['breed']
+    print breed
+    petfinder = breed_list['petfinder']
+    print petfinder
+    breeds = petfinder['breeds']
+    print breeds
+    breed_type = breeds['breed']
+    print breed_type
+    breed_name = breed_type
+    print breed_name
+    for breed in breed_name:
+        print breed['$t']
+        reptile_breeds.append(str(breed['$t']))
+    print reptile_breeds
+    context = {
+        'breed_list': breed_list,
+        'reptile_breeds': reptile_breeds,
+    }
+    return render(request, 'petfinder_api/reptile_breeds.html', context)
